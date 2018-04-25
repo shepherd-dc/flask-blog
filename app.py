03bfe768bc5a3097a0ab021ae67513fce3daf865
shepherd_dc <shepherd_dc@aliyun.com>
2018-04-25 11:26:41 +0800

    extends block

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import config
 
 app = Flask(__name__)
@@ -7,6 +7,20 @@ app.config.from_object(config)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        pass
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        pass
 
 if __name__ == '__main__':
     app.run()
